152c0a094d8b307ce48c6124c7207aade5a67e44
kouwa stephane <kksasteph888@gmail.com>
2022-07-11 09:08:16 +0200

    MAJ

--- src/Plugin/Layout/Sections/ClothingslayoutsTitre.php
+++ src/Plugin/Layout/Sections/ClothingslayoutsTitre.php
@@ -5,6 +5,8 @@ namespace Drupal\clothingslayouts\Plugin\Layout\Sections;
 use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
 use Drupal\formatage_models\FormatageModelsThemes;
 use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
+use Drupal\user\Entity\Role;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * A very advanced custom layout.
@@ -52,6 +54,9 @@ class ClothingslayoutsTitre extends FormatageModelsSection {
   function defaultConfiguration() {
     return [
       'region_tag_title' => 'h3',
+      'layoutrestrictions' => [
+        'roles' => []
+      ],
       'infos' => [
         'builder-form' => true,
         'info' => [
@@ -68,6 +73,42 @@ class ClothingslayoutsTitre extends FormatageModelsSection {
         ]
       ]
     ] + parent::defaultConfiguration();
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    
+    $roles = [];
+    foreach (Role::loadMultiple() as $role) {
+      $roles[$role->id()] = $role->label();
+    }
+    $form['layoutrestrictions'] = [
+      
+      '#type' => 'details',
+      '#title' => 'layout restrictions ...',
+      '#open' => false,
+      '#tree' => true
+    ];
+    $form['layoutrestrictions']['roles'] = [
+      '#type' => 'checkboxes',
+      '#title' => 'selectionner les roles',
+      '#options' => $roles,
+      '#default_value' => $this->configuration['layoutrestrictions']['roles']
+    ];
+    return $form;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+    $this->configuration['layoutrestrictions'] = $form_state->getValue('layoutrestrictions');
   }
   
 }
